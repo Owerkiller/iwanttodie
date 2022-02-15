f9d9ad76817886ed06e099b9f06a35f14647d826
Owerkiller <ivanov_es_7856@mail.ru>
2022-02-15 22:11:00 +0300

    input/output matrix/array of matrices

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -1,8 +1,9 @@
 #include "matrix.h"
+#include <stdio.h>
 
 matrix getMemMatrix(int nRows, int nCols) {
     int **values = (int **) malloc(sizeof(int) * nRows);
-    for (int i = 0; i < nRows; ++i) {
+    for (int i = 0; i < nRows; i++) {
         values[i] = (int *) malloc(sizeof(int) * nCols);
     }
     return (matrix) {values, nRows, nCols};
@@ -24,4 +25,30 @@ void freeMemMatrix(matrix m) {
 void freeMemMatrices(matrix *ms, int nMatrices) {
     for (size_t i = 0; i < nMatrices; i++)
         freeMemMatrix(ms[i]);
+}
+
+void inputMatrix(matrix m) {
+    for (size_t i=0; i<m.nRows; i++)
+        for (int j = 0; j < m.nCols; j++) {
+            scanf ("%d", &m.values[j][i]);
+        }
+}
+
+void inputMatrices (matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++) {
+        inputMatrix(ms[i]);
+    }
+}
+
+void outputMatrix( matrix m){
+    for (size_t i = 0; i < m.nRows; i++) {
+        for (size_t j = 0; j<m.nCols; j++)
+            printf("%d ", m.values[i][j]);
+        printf("/n");
+    }
+}
+
+void outputMatrices(matrix* ms, int nMatrices){
+    for (int i=0; i<nMatrices; i++)
+        outputMatrix(ms[i]);
 }
